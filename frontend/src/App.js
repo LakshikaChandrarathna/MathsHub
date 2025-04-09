dd78452af57cff938e0e9005b1bb5bb07735f582
lakshika chandrarathna <ulakshika.c1218@gmail.com>
2025-04-09 19:52:51 +0530

    changes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Login,Signup} from "./Routes.js";
 import UploadFiles from "./pages/admin/UploadFiles.js";
 import Navbar from "./components/admin/Navbar.js";
+import Sidebar from "./components/admin/Sidebar.js";
+import Home from "./pages/Home.js";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path='/Signup' element={<Signup />} />
         <Route path='/UploadFiles' element={<UploadFiles />} />
         <Route path='/Navbar' element={<Navbar />} />
+        <Route path='/Sidebar' element={<Sidebar />} />
+        <Route path='/Home' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
